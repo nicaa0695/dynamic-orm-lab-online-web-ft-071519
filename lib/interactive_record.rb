0b8dd604323fcd0e9fec54c66ab67effd626bd08
Veronica Lopez <nicaa0695@gmail.com>
2019-08-20 16:28:55 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,14 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+  def self.create_table 
+    sql = <<-SQL 
+    CREATE TABLE IF NOT EXISTS students(
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    grade INTEGER
+    )
+    SQL
+    DB[:conn].execute(sql)
+  end
 end
