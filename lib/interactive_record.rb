61c31d6551dadca7cad75862ab65a9d93512c5f9
Veronica Lopez <nicaa0695@gmail.com>
2019-08-20 16:41:54 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -18,5 +18,11 @@ class InteractiveRecord
     end 
     column_names.compact
   end
+  
+  def initialize(options={})
+    options.each do |property, value|
+      self.send("#{property}=", value)
+    end 
+  end 
     
 end
